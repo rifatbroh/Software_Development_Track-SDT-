a3472abac2380fd0eadb3c3aa8305dbe9b049fd8
rifatbroh <163645638+rifatbroh@users.noreply.github.com>
2024-10-27 19:36:37 +0600

    Add files via upload

---
+++ python/day_1/2.data_type_variable.py
@@ -0,0 +1,36 @@
+x = 5
+y = 'bangladesh'
+z = 1.343
+
+print(type(x)) # this is int
+print(type(y)) # this is str
+print(type(z)) # this is float
+
+"""
+python(hello world)
+    data type :
+        Number
+        string 
+        list
+        tupple
+        dictionary
+        complex number
+"""
+
+# variable data assign
+name = "hello bangladesh, are you okey ?"
+number = 678
+pi = 3.1416
+print(type(pi))
+
+# multiple assingment
+
+#rifat, prome, razil, moneem = 3.99 # -> same
+#rifat, prome, razil, moneem = 3.14, "hello", 88, "bangla"
+
+rifat, prome, razil, moneem = "sadia", "soha", "samiya", "nahida"
+# string e always double cotation dite hobe je 
+
+s = "we are overcome"
+print(s)
+print ("hello " + "world")
