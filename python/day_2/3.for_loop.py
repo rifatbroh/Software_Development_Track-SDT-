59e2bea20482097101a93ec41731b923e51fb677
rifatbroh <163645638+rifatbroh@users.noreply.github.com>
2024-10-28 00:32:24 +0600

    Add files via upload

---
+++ python/day_2/3.for_loop.py
@@ -0,0 +1,21 @@
+for letter in 'python' :
+    print("the letter is :", letter)
+
+friends = ['Rahim', 'rifat', 'prome']
+for i in friends:
+    print("Happy new year: ", i)
+
+
+for i in range(10):
+    print(i)
+
+for i in range(5, 10):
+    print(i)
+
+
+prime = [2, 3, 4, 5, 6, 7]
+cnt = 0
+
+for i in prime:
+    cnt = cnt + 1
+print("Total number of element: ", cnt)
