53a2d61451b2791bb51aa24af149958f2686265b
rifatbroh <163645638+rifatbroh@users.noreply.github.com>
2024-10-27 19:35:35 +0600

    Add files via upload

---
+++ python/day_1/1.hello.py
@@ -0,0 +1,18 @@
+# this is python broh !
+
+print("hello world")
+
+'''
+    amra ajke theke python shikbo
+    target -> just 3 week times !
+'''
+
+x = 21
+y = 19
+print(x+y)
+
+if x>y:
+    # indentation maintain
+    print("x is grater then y")
+else:
+    print("y is grater then x")
