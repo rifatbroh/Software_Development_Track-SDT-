6efa2fc473416cb4836ceff7f52205593bf8d743
rifatbroh <163645638+rifatbroh@users.noreply.github.com>
2024-11-01 20:31:35 +0600

    Add files via upload

---
+++ python/day_3/class.py
@@ -0,0 +1,19 @@
+"""
+    define a class
+"""
+class Student:
+    name = "rifat"
+    id = 39
+    section = 'A'
+
+"""
+    define a object 
+"""
+info = Student()
+
+"""
+    Relation between them:
+"""
+print(info.name)
+print(info.id)
+print(info.section)
