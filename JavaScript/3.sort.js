9f2136a6b473d9e14cfda12b7cdea712a35b5191
rifatbroh <163645638+rifatbroh@users.noreply.github.com>
2025-02-02 18:15:08 +0600

    Add files via upload

---
+++ JavaScript/3.sort.js
@@ -0,0 +1,8 @@
+/*
+    Sort array in ascending order
+*/
+var arr = [20, 19, 18, 17, 16, 15, 14, 13, 12, 
+        11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1];
+
+arr.sort((a, b) => a - b);
+console.log("Sorted array:", arr);
