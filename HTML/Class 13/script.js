800a9c07bad7ffce793028591ea9652fcf8a46e6
rifatbroh <163645638+rifatbroh@users.noreply.github.com>
2025-02-03 18:09:19 +0600

    Add files via upload

---
+++ HTML/Class 13/script.js
@@ -0,0 +1,18 @@
+/**
+ *  id diye object select korbo
+ */
+const a = document.getElementById("demo");
+a.innerHTML = "Hello Every One";
+
+/**
+ *  tag name diye object select korbo
+ */
+document.getElementsByTagName("p");
+document.getElementsByTagName("a");
+/**
+ *  class diye object select korbo
+ */
+const b = document.querySelector(".cl_1");
+if (b) {
+    b.innerHTML = "Today I learn Js with DOM";
+}
