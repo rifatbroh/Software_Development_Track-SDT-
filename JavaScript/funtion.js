aac34b6e6baf8dcd2ef30c66b25d141b597494fb
rifatbroh <163645638+rifatbroh@users.noreply.github.com>
2025-02-03 16:44:47 +0600

    Add files via upload

---
+++ JavaScript/funtion.js
@@ -0,0 +1,16 @@
+function sum (a, b) {
+    return a+b;
+}
+var x = sum(5,6);
+console.log(x);
+
+
+/*
+    ES6
+
+*/
+const fun = (a, b) => a+b;
+console.log(fun(19,1));
+
+const ssun = (a,b) =>a+b;
+console.log(ssun(1,1));
