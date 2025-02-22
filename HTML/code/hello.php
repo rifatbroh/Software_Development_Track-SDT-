d24023b374c6e9cd3459f46632dcd8265bbb3d54
rifatbroh <163645638+rifatbroh@users.noreply.github.com>
2025-02-22 15:42:49 +0600

    Add files via upload

---
+++ HTML/code/hello.php
@@ -0,0 +1,15 @@
+<?php include_once('header.php');  ?>
+
+                <h2>Welcome to our Website</h2>
+                <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Dicta, iusto eligendi! Sit nihil nemo ipsa eveniet, optio quod
+                    fugit quidem, incidunt, molestiae quia possimus tempore? Ducimus
+                    ad iure nihil hic autem quisquam quam veritatis, sequi neque quae 
+                    numquam rem iusto eaque? Nihil odio nulla molestiae dicta ab voluptatem,
+                    laudantium eligendi.
+                </p>
+                <a href="" class="btn">Contact Us</a>
+
+<?php include_once('footer.php');  ?>
+
+<!-- website templating -->
